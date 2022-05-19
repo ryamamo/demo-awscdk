e193690be1e1667e19b27e01027f52ea88712660
ryamamo <carefree.shima@gmail.com>
2022-05-19 15:15:36 +0900

    add igw

--- lib/demo-stack.ts
+++ lib/demo-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Vpc } from './resource/vpc';
 import { Subnet } from './resource/subnet';
+import { InternetGateway } from './resource/internetGateway';
 
 export class DemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -12,5 +13,8 @@ export class DemoStack extends Stack {
 
     const subnet = new Subnet(vpc.vpc);
     subnet.createResources(this, props);
+
+    const igw = new InternetGateway(vpc.vpc);
+    igw.createResources(this, props);
   }
 }
